1a122a2905f68c9a6a8ff9c787bb15898dadf993
Toxic254 <cyrus_g@outlook.com>
2022-08-23 14:57:47 +0300

    Infered the `RootState` and `AppDispatch` types from the store itself

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -6,3 +6,7 @@ export const store = configureStore({
     counter: counterSlice.reducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
